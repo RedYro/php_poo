655223a4c0ef5a75f67f3ec61deeed51c752bc2b
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-06-27 16:57:21 +0200

    Updates PHP_POO

--- 01_Classes_Objets/Panier.php
+++ 01_Classes_Objets/Panier.php
@@ -1,8 +1,27 @@
 <?php
+    /** La programmation orientée objet (POO) permet de :
+    *     - Avoir une meilleure organisation et construction des  projets
+    *     - Facilite la maintenance du code
+    *     - Offre une grande souplesse pour faire évoluer le projet sans avoir à tout réécrire
+
+    * En programmation, objet est le nom que l’on donne à l'élément : qui se trouve dans une variable dans lequel on peut stocker des valeurs et auquel on peut demander de faire des actions. 
+
+    *  Pour faire simple, c’est un regroupement de valeurs et de fonctions.
+    *  Pour obtenir un objet, il faut demander au langage de le créer et de nous le donner pour qu’on puisse le manipuler. Pour faire ça, on précise au langage le nom de l’élément que l’on veut manipuler, c’est-à-dire la classe.
+    *  Une classe permet de produire plusieurs objets. Par convention, une classe sera nommée avec la première lettre en MAJUSCULE.
+    *  La classe représente un modèle de données qui définit la structure commune à tous les objets créés à partir d'elle. La classe constitue donc une sorte de moule à travers laquelle plusieurs objets du même type et de mêmes structures peuvent être créés.
+    *  Une classe représente une entité ( le modéle qu'elle doit suivre) elle a ses services (= méthodes : ce qu'elle propose et ce qu'elle permet de faire ) et elle a les attributs ( = propritétés :  ces spécificités ) 
+    * 
+    *  Pour en savoir plus : 
+    *   - https://blog.hubspot.fr/website/programmation-orientee-objet#:~:text=La%20programmation%20orient%C3%A9e%20objet%2C%20ou,des%20instances%20individuelles%20d'objets.
+    * 
+    *  Pour définir  et créer une classe : on utilise le mot clé class suivit du nom de la classe (avec une lettre majuscule au début et sur chaque début d'un nouveau mot dans le nom de la classe) 
+    *  exp: MyClass => voir le dossier 01_Classes_Objets. */
+    
     class Panier{
 
     }
-    
+
     $date = new DateTime();
     $date2 = new DateTime();
 ?>
