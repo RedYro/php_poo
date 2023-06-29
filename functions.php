348adfc6a7aaa6323a100557446c8f82547f1bfc
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-06-29 16:30:01 +0200

    Updates PHP_POO

--- functions.php
+++ functions.php
@@ -2,9 +2,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <?php
 function debug($var){
-
      echo '<pre class="border border-dark bg-light text-primary w-50 p-3">';
      var_dump($var); 
      echo '</pre>';
-
 }
