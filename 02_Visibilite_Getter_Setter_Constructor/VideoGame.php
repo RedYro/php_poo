7b189afe90f0a10391122313df7867e8435879b8
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-06-29 12:50:09 +0200

    Updates PHP_POO

--- 02_Visibilite_Getter_Setter_Constructor/VideoGame.php
+++ 02_Visibilite_Getter_Setter_Constructor/VideoGame.php
@@ -1,5 +1,13 @@
 <?php
     require_once("../functions.php");
 
+    // CONSTRUCTOR // 
 
+    class VideoGame{
+        private string $videoGameName;
+        private string $console;
+        private float $price;
+
+        // Le constructeur est une méthode particulière dans une classe, elle est définie avec le nom "___construct()" qui va indiquer comment la classe doit créer l'objet
+    }
 ?>
