dbdf15ccbb26d51201fe858c99f4583929ec3607
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-07-07 14:39:35 +0200

    Updates PHP_POO

--- 04_Namespace/Namespace_ab.php
+++ 04_Namespace/Namespace_ab.php
@@ -1,4 +1,25 @@
 <?php
-    // namespace : on peut y stocker des "méthodes" et des "class", considéré comme un "dossier virtuel" 
+    // namespace : on peut y stocker des "méthodes" et des "class", considéré comme un "dossier virtuel"
+    
+    namespace A{
+        function ville(){
+            return "<p>Paris</p>";
+        }
 
+        function strlen(){
+            return "<p>Fonction strlen de A</p>";
+        }
+    }
+
+    //----------------------------------------------------------------
+    
+    namespace B{
+        function ville(){
+            return "<p>Nante</p>";
+        }
+
+        function strlen(){
+            return "<p>Fonction strlen de B</p>";
+        }
+    }
 ?>
